b28e29103930050b9ab059c047f2328cd56f3bfa
Ales <aleskruba@seznam.cz>
2024-03-31 15:21:02 +0200

    scroll bars

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
   };
 
   return (
-    <div className="w-screen h-screen bg-cover bg-center flex items-center justify-center pt-4 bg-gradient-to-t  from-black  to-very-dark-purple">
+    <div className="w-screen h-screen overflow-y-auto custom-scrollbar   bg-cover bg-center flex items-center justify-center pt-4 bg-gradient-to-t  from-black  to-very-dark-purple">
       <div id='contact' className="h-[60%] w-[80%] relative bg-center rounded-xl border border-white">
         <motion.div className="flex justify-start items-start  pt-2 md:justify-center md:items-start md:mt-8 md:ml-8 h-full" 
                     initial={{ y: "0%" }} 
