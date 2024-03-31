96678631791c3f6bc03362016ac1aef687bdd9fb
Ales <aleskruba@seznam.cz>
2024-03-31 15:28:10 +0200

    scroll bars II

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -14,8 +14,8 @@ const Page = () => {
 
   return (
     <div className="w-screen h-screen overflow-y-auto custom-scrollbar   bg-cover bg-center flex items-center justify-center pt-4 bg-gradient-to-t  from-black  to-very-dark-purple">
-      <div id='contact' className="h-[60%] w-[80%] relative bg-center rounded-xl border border-white">
-        <motion.div className="flex justify-start items-start  pt-2 md:justify-center md:items-start md:mt-8 md:ml-8 h-full" 
+      <div id='contact' className="h-[50%] w-[80%] relative bg-center rounded-xl border border-white">
+        <motion.div className="flex justify-start items-start  lg:pt-2 md:justify-center md:items-baseline lg:mt-8 md:ml-8 h-full" 
                     initial={{ y: "0%" }} 
                     animate={{ y: "25%" }} 
                     transition={{ duration: 1.5 }}
--- components/ContactFrom.tsx
+++ components/ContactFrom.tsx
@@ -8,10 +8,10 @@ const ContactForm = ({animationDone}:any) => {
 
 
 return (
-<div className={`flex flex-col justify-center items-baseline  md:bg-transparent px-4 mx-2 md:mx-0 text-xl md:text-2xl lg:text-3xl mb-2 text-yellow-500 font-normal ${!animationDone ? 'border border-solid border-white ' : 'gap-4'} text-gold-500 py-4 rounded-lg shadow-md`}>
+<div className={`flex flex-col justify-center items-baseline  md:bg-transparent px-4 mx-2 md:mx-0 text-xl md:text-2xl lg:text-3xl mb-2 text-yellow-500 font-normal ${!animationDone ? 'border border-solid border-white ' : ''} text-yellow-500 py-4 rounded-lg shadow-md`}>
       <div className="flex  items-center justify-around gap-8 ">
         <FaEnvelope className="mb-2" />
-        <a href="mailto:aleskruba@seznam.cz" className="text-gold-500 hover:text-yellow-400">aleskruba@seznam.cz</a>
+        <a href="mailto:aleskruba@seznam.cz" className="text-yellow-500 hover:text-yellow-400">aleskruba@seznam.cz</a>
 
       </div>
       <div className="flex  items-center justify-around gap-8 mt-2">
